8f294dbbc53438c2d44b3d6e66092766324b5875
ntejdev <ntejdev@gmail.com>
2016-10-30 03:28:29 -0700

    master commit

---
+++ app/src/main/java/www/ntej/com/simplenotes/Main3Activity.java
@@ -0,0 +1,13 @@
+package www.ntej.com.simplenotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
